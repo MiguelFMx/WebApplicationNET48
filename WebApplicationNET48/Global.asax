30bb79a67c26bc2e856e23d21cd7530ec10b6c45
MiguelF <freyde.miguel@gmail.com>
2022-09-23 09:06:04 -0700

    Agregar archivos de proyecto.

---
+++ WebApplicationNET48/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationNET48.Global" Language="C#" %>
---
+++ WebApplicationNET48/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationNET48.Startup))]
+namespace WebApplicationNET48
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
